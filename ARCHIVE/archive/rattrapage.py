e524afe2c9577a2fdbc336eea7addf682de0ed32
Ilyes Kebairi <ilyes.kebairi@gmail.com>
2025-12-12 16:09:30 +0100

    Add files via upload
    Fichier rattrapge

--- ARCHIVE/archive/rattrapage.py
+++ ARCHIVE/archive/rattrapage.py
@@ -6,6 +6,7 @@ Projet TL : parser - requires Python version >= 3.10
 
 import sys
 from math import factorial
+
 assert sys.version_info >= (3, 10), "Use Python 3.10 or newer !"
 
 import lexer
@@ -17,23 +18,28 @@ from definitions import V_T, str_attr_token
 _current_token = V_T.END
 _value = None  # attribut du token renvoyé par le lexer
 
+
 #####
 # Fonctions génériques
 
 class ParserError(Exception):
     pass
 
+
 def unexpected_token(expected):
     return ParserError("Found token '" + str_attr_token(_current_token, _value) + "' but expected " + expected)
 
+
 def get_current():
     return _current_token
+
 
 def init_parser(stream):
     global _current_token, _value
     lexer.reinit(stream)
     _current_token, _value = lexer.next_token()
     # print("@ init parser on",  repr(str_attr_token(_current, _value)))  # for DEBUGGING
+
 
 def consume_token(tok):
     # Vérifie que le prochain token est tok ;
@@ -46,186 +52,154 @@ def consume_token(tok):
         _current_token, _value = lexer.next_token()
         return old
 
+def recover(suiv):
+    curr = get_current()
+    while curr not in suiv and curr != V_T.END and curr != V_T.SEQ:
+        consume_token(curr)
+        curr = get_current()
+
 #########################
 ## Parsing de input et exp
 
-def recover(suiv):
-    
-    while get_current() not in suiv and get_current() != V_T.END:
-        consume_token(get_current())
-    
-    if get_current() == V_T.END:
-        raise ParserError("Parsing error: unexpected end of input.")
-    
-    
-    return get_current()
+results = []
 
-def parse_input(L=[]):
-    match get_current():
-        case V_T.END:
-            return L
-        case V_T.NUM | V_T.CALC | V_T.OPAR | V_T.SUB:
-            n = parse_exp5(L)
+def parse_input():
+    global results
+    results = []
+    tok = get_current()
+    if tok == V_T.END:
+        return results
+    while True:
+        # Consommer tous les séparateurs SEQ consécutifs
+        while get_current() == V_T.SEQ:
             consume_token(V_T.SEQ)
-            L = L + [n]
-            L = parse_input(L)
-
-            return L
-        case _:
-            recover([V_T.NUM, V_T.CALC, V_T.OPAR, V_T.SUB,V_T.END])
-            parse_input(L)
-
-def parse_exp5(L):
-    match get_current():
-        case V_T.NUM | V_T.CALC | V_T.OPAR | V_T.SUB:
-            n_1 = parse_exp4(L)
-            n = parse_Z(L,n_1)
-            return n
-        case _:
-            recover([V_T.NUM, V_T.CALC, V_T.OPAR, V_T.SUB])
-            parse_exp5(L)
-
-def parse_Z(L,n_1):
-    match get_current():
-        case V_T.ADD | V_T.SUB:
-            n_2 = parse_exp5_bis(L,n_1)
-            n_3 = parse_Z(L,n_2)
-            return n_3
-        case V_T.CPAR | V_T.SEQ:
-            return n_1
-        case _:
-            recover([V_T.ADD, V_T.SUB, V_T.CPAR, V_T.SEQ])
-            parse_Z(L,n_1)
-
-def parse_exp5_bis(L,n_1):
-    match get_current():
-        case V_T.ADD:
-            consume_token(V_T.ADD)
-            n_2 = parse_exp4(L)
-            return n_1 + n_2
-        case V_T.SUB:
-            consume_token(V_T.SUB)
-            n_2 = parse_exp4(L)
-            return n_1 - n_2
-        case _:
-            recover([V_T.ADD, V_T.SUB])
-            parse_exp5_bis(L,n_1)
-
-def parse_exp4(L):
-    match get_current():
-        case V_T.NUM | V_T.CALC | V_T.OPAR | V_T.SUB:
-            n_1 = parse_exp3(L)
-            n = parse_Y(L,n_1)
-            return n
-        case _:
-            recover([V_T.NUM, V_T.CALC, V_T.OPAR, V_T.SUB])
-            parse_exp4(L)
-
-def parse_Y(L,n_1):
-    match get_current():
-        case V_T.MUL | V_T.DIV:
-            n_2 = parse_exp4_bis(L,n_1)
-            n_3 = parse_Y(L,n_2)
-            return n_3
-        case V_T.CPAR | V_T.ADD | V_T.SUB | V_T.SEQ:
-            n_3 = n_1
-            return n_3
-        case _:
-            recover([V_T.MUL, V_T.DIV, V_T.CPAR, V_T.ADD, V_T.SUB, V_T.SEQ])
-            parse_Y(L,n_1)
-
-def parse_exp4_bis(L,n_1):
-    match get_current():
-        case V_T.MUL:
-            consume_token(V_T.MUL)
-            n_2 = parse_exp3(L)
-            n = n_1 * n_2
-            return n
-        case V_T.DIV:
-            consume_token(V_T.DIV)
-            n_2 = parse_exp3(L)
-            return n_1 / n_2
-        case _:
-            recover([V_T.MUL, V_T.DIV])
-            parse_exp4_bis(L,n_1)
-
-def parse_exp3(L):   
-    match get_current():
-        case V_T.SUB:
-            consume_token(V_T.SUB)
-            n_1 = parse_exp3(L)
-            n = -1 * n_1
-            return n
-        case V_T.NUM | V_T.CALC | V_T.OPAR:
-            n = parse_exp2(L)
-            return n 
-        case _:
-            recover([V_T.SUB, V_T.NUM, V_T.CALC, V_T.OPAR])
-            parse_exp3(L)
-
-def parse_exp2(L):
-    match get_current():
-        case V_T.NUM | V_T.CALC | V_T.OPAR:
-            n_1 = parse_exp1(L)
-            n = parse_exp2_bis(n_1)
-            return n
-        case _:
-            recover([V_T.NUM, V_T.CALC, V_T.OPAR])
-            parse_exp2(L)
-
-def parse_exp2_bis(n):
-    match get_current():
-        case V_T.FACT:
-            consume_token(V_T.FACT)
-            return factorial(int(n))
-        case V_T.CPAR | V_T.MUL | V_T.DIV | V_T.ADD | V_T.SUB | V_T.SEQ:
-            return n
-        case _:
-            recover([V_T.FACT, V_T.CPAR, V_T.MUL, V_T.DIV, V_T.ADD, V_T.SUB, V_T.SEQ])
-            parse_exp2_bis(n)
-
-def parse_exp1(L):
-    match get_current():
-        case V_T.NUM | V_T.CALC | V_T.OPAR:
-            n_1 = parse_exp0(L)
-            n = parse_exp1_bis(L,n_1)
-            return n
-        case _:
-            recover([V_T.NUM, V_T.CALC, V_T.OPAR])
-            parse_exp1(L)
-
-def parse_exp1_bis(L,n):
-    match get_current():
-        case V_T.POW:
-            consume_token(V_T.POW)
-            n_1 = parse_exp1(L)
-            return n**n_1
-        case V_T.CPAR | V_T.FACT | V_T.MUL | V_T.DIV | V_T.ADD | V_T.SUB | V_T.SEQ:
-            return n
-        case _:
-            recover([V_T.POW, V_T.CPAR, V_T.FACT, V_T.MUL, V_T.DIV, V_T.ADD, V_T.SUB, V_T.SEQ])
-            parse_exp1_bis(L,n)
-
-def parse_exp0(L):
-    match get_current():
-        case V_T.NUM:
-            n = consume_token(V_T.NUM)
-            return n
-        case V_T.CALC:
-            i = consume_token(V_T.CALC)
-            print(i)
-            n = L[i-1]
-            return n
-        case V_T.OPAR:
-            consume_token(V_T.OPAR)
-            n = parse_exp5(L)
-            consume_token(V_T.CPAR)
-            return n
-        case _:
-            recover([V_T.NUM, V_T.CALC, V_T.OPAR])
-            parse_exp0(L)
+            continue
+        if get_current() == V_T.END:
+            break
+        val = parse_exp5()
+        results.append(val)
+        recover({V_T.SEQ})
+        
+    return results
 
 
+def parse_exp5():
+    cumul = parse_exp4()
+    n = parse_exp5_prime(cumul)
+    return n
+
+def parse_exp5_prime(cumul):
+    tok = get_current()
+    if tok == V_T.ADD:
+        consume_token(tok)
+        # Vérifier si on peut parser une expression
+        if get_current() in (V_T.END, V_T.SEQ):
+            return cumul  # Ignorer l'opérateur si l'opérande manque
+        n2 = parse_exp4()
+        return parse_exp5_prime(cumul + n2)
+    elif tok == V_T.SUB:
+        consume_token(tok)
+        # Vérifier si on peut parser une expression
+        if get_current() in (V_T.END, V_T.SEQ):
+            return cumul  # Ignorer l'opérateur si l'opérande manque
+        n2 = parse_exp4()
+        return parse_exp5_prime(cumul - n2)
+    else:
+        return cumul
+
+
+def parse_exp4():
+    cumul = parse_exp3()
+    n = parse_exp4_prime(cumul)
+    return n
+
+
+def parse_exp4_prime(cumul):
+    tok = get_current()
+    if tok == V_T.MUL:
+        consume_token(tok)
+        #On verifie si on peut parser une expression (pour éviter la division par zéro)
+        if get_current() in (V_T.END, V_T.SEQ):
+            return cumul  # Ignorer l'opérateur si l'opérande manque
+        n2 = parse_exp3()
+        return parse_exp4_prime(cumul * n2)
+    elif tok == V_T.DIV:
+        consume_token(tok)
+        # On verifie si on peut parser une expression (pour éviter la division par zéro)
+        if get_current() in (V_T.END, V_T.SEQ):
+            return cumul  #On ignore l'opérateur si le nombre suivant est manquant
+        n2 = parse_exp3()
+        return parse_exp4_prime(cumul / n2)
+    else:
+        return cumul
+
+
+def parse_exp3():
+    tok = get_current()
+    if tok == V_T.SUB:
+        consume_token(tok)
+        n = parse_exp3()
+        return -n
+    elif tok in (V_T.NUM, V_T.CALC, V_T.OPAR):
+        return parse_exp2()
+    else:
+        recover((V_T.SUB,V_T.NUM, V_T.CALC,V_T.OPAR))
+        #Note pour souvenir : si après récupération on est à END ou SEQ, on ne peut plus parser une expression
+        if get_current() in (V_T.END, V_T.SEQ):
+            return 0 
+        return parse_exp3()
+
+
+def parse_exp2():
+    cumul = parse_exp1()
+    n = parse_exp2_prime(cumul)
+    return n
+
+
+def parse_exp2_prime(cumul):
+    tok = get_current()
+    if tok == V_T.FACT:
+        consume_token(tok)
+        return parse_exp2_prime(factorial(cumul))
+    else:
+        return cumul
+
+
+def parse_exp1():
+    base = parse_exp0()
+    return parse_exp1_prime(base)
+
+
+def parse_exp1_prime(base):
+    if get_current() == V_T.POW:
+        consume_token(V_T.POW)
+        exp = parse_exp1()
+        return pow(base, exp)
+    return base
+
+
+def parse_exp0():
+    tok = get_current()
+    if tok == V_T.NUM:
+        val = consume_token(tok)
+        return val
+    if tok == V_T.CALC:
+        idx = consume_token(V_T.CALC)
+        if idx <= 0 or idx > len(results):
+            raise ValueError(f"Invalid calcul reference #{idx}")
+        return results[idx - 1]
+    elif tok == V_T.OPAR:
+        consume_token(tok)
+        val = parse_exp5()
+        while get_current() == V_T.CPAR:
+            consume_token(get_current())
+        return val
+    else:
+        recover((V_T.NUM, V_T.CALC, V_T.OPAR))
+        if get_current() == V_T.END:
+            return 0 #Ou raise parseError
+
+        return parse_exp0()
 
 
 #####################################
@@ -240,6 +214,7 @@ def parse(stream=sys.stdin):
     consume_token(V_T.END)
     return l
 
+
 #####################################
 ## Test depuis la ligne de commande
 
@@ -250,6 +225,3 @@ if __name__ == "__main__":
         print("@ Input OK ")
     else:
         print("@ result = ", repr(result))
-
-
-#########
